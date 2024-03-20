2fcbe897afb1de7b5224d6a8a5a6dd4c07f71ebd
Wes Modes <wmodes@gmail.com>
2024-03-20 13:33:31 -0700

    WIP

--- js/tattoo.js
+++ js/tattoo.js
@@ -5,41 +5,57 @@ var maxPages = 18
 $(document).ready(function() {
   displayPage(currentPage); // Display the initial page
   prepNav(); // Prepare navigation buttons and event listeners
+
+  // Function to display a page
+  function displayPage(pageNumber) {
+    currentPage = pageNumber;
+    $('.page').hide();
+    $('.page' + currentPage).fadeIn();
+  }
+
+  // Add navigation buttons to each page
+  function prepNav() {
+    $('.page').each(function() {
+        $(this).append('<button class="prev"></button>');
+        $(this).append('<button class="next"></button>');
+    });
+
+    $('.prev').click(function() {
+        prevPage();
+    });
+
+    $('.next').click(function() {
+        nextPage();
+    });
+  }
+
+  // Event handling to go to the previous page
+  function prevPage() {
+    if(currentPage === 1) {
+        displayPage(maxPages); // Assuming there are 5 pages, adjust if there are more or fewer pages
+    } else {
+        displayPage(currentPage - 1);
+    }
+  }
+
+  // Event handling to go to the next page
+  function nextPage() {
+    if(currentPage === maxPages) { // Assuming there are 5 pages, adjust if there are more or fewer pages
+        displayPage(1);
+    } else {
+        displayPage(currentPage + 1);
+    }
+  }
+
+  $(window).resize(function() {
+    var windowWidth = $(window).width();
+    var originalWidth = 1920; // Assuming the original width of your elements is 1920px
+    var scaleFactor = windowWidth / originalWidth;
+
+    $('.page-contents').css({
+        'transform': 'scale(' + scaleFactor + ')',
+        'transform-origin': 'top left'
+    });
+  }).trigger('resize');
+
 });
-
-function displayPage(pageNumber) {
-  currentPage = pageNumber;
-  $('.page').hide();
-  $('.page' + currentPage).fadeIn();
-}
-
-function prepNav() {
-  $('.page').each(function() {
-      $(this).append('<button class="prev"></button>');
-      $(this).append('<button class="next"></button>');
-  });
-
-  $('.prev').click(function() {
-      prevPage();
-  });
-
-  $('.next').click(function() {
-      nextPage();
-  });
-}
-
-function prevPage() {
-  if(currentPage === 1) {
-      displayPage(maxPages); // Assuming there are 5 pages, adjust if there are more or fewer pages
-  } else {
-      displayPage(currentPage - 1);
-  }
-}
-
-function nextPage() {
-  if(currentPage === maxPages) { // Assuming there are 5 pages, adjust if there are more or fewer pages
-      displayPage(1);
-  } else {
-      displayPage(currentPage + 1);
-  }
-}
